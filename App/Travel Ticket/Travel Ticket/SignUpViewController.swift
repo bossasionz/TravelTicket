20419d28128562b87d0efb18f8ea16644c6e8e8d
comart <comart@rba1f403-007.local>
2018-09-28 15:42:06 +0700

    Firebase

---
+++ App/Travel Ticket/Travel Ticket/SignUpViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpViewController.swift
+//  Travel Ticket
+//
+//  Created by comart on 28/9/2561 BE.
+//  Copyright © 2561 student. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
