06f37900dcf460aa134436e8f03ec7a729afc41f
student <student@rba7f403-009.local>
2018-10-08 16:08:41 +0700

    Table View and pass refer done

--- App/Travel Ticket/Travel Ticket/TripPromotionViewController.swift
+++ App/Travel Ticket/Travel Ticket/TripPromotionViewController.swift
@@ -10,36 +10,44 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 
+struct Trips {
+    var tripname:String
+}
+
+
 class TripPromotionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var callListTableView: UITableView!
     
     let ref = Firestore.firestore().collection("TripPromotion")
     
-    var doccount = 0
-    
-    var tripname = [""]
-//    var tripprice = [""]
+    var tripsArray = [Any]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ref.getDocuments(){ (Snapshot , err ) in
-            var count = 0
-            for document in Snapshot!.documents {
-                self.tripname.append(document.documentID)
-                count += 1
-            }
-            print(count)
-            self.doccount = count
-            print(self.doccount)
+        ref.getDocuments(){ ( snapshot, err) in
+            for document in (snapshot?.documents)! {
+                if let Title = document.documentID as? String{
+                    print(Title)
 
+                    self.tripsArray.append(Title)
+
+                }
+            }
+            
+            DispatchQueue.main.async {
+                print(self.tripsArray.count)
+                self.callListTableView.reloadData()
+            }
         }
         
-                callListTableView.dataSource = self
-                callListTableView.delegate = self
+
+        callListTableView.dataSource = self
+        callListTableView.delegate = self
+
     }
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
@@ -48,16 +56,31 @@ class TripPromotionViewController: UIViewController, UITableViewDataSource, UITa
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         
-        return 5
+        return tripsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let trip = tripsArray[indexPath.row]
+        cell.textLabel?.text = trip as? String
         
-        cell.textLabel?.text = "\(doccount)"
         
+       
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let detailvc = self.storyboard?.instantiateViewController(withIdentifier: "detailvc") as! detailViewController
+        let selectedRow = tableView.indexPathForSelectedRow!
+        let currectRow = tableView.cellForRow(at: selectedRow) as! UITableViewCell
+        detailvc.trip = (currectRow.textLabel?.text as? String)!
+        
+        
+        DispatchQueue.main.async(execute: {
+            self.present(detailvc, animated: true, completion: nil)
+        })
     }
     
     
