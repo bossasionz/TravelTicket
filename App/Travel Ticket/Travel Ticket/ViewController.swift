00840966fd516126376c938b21adfe41da86c306
Hong <siripong@siripongs-macbook-pro.local>
2018-10-09 12:20:55 +0700

    Login

--- App/Travel Ticket/Travel Ticket/ViewController.swift
+++ App/Travel Ticket/Travel Ticket/ViewController.swift
@@ -22,21 +22,55 @@ class ViewController: UIViewController {
     }
     @IBAction func Signin_Btn(_ sender: Any) {
         
-        Auth.auth().signIn(withEmail: Email.text!, password: Password.text!) { (user, error) in
-            if error == nil{
+        if self.Email.text == "" || self.Password.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            Auth.auth().signIn(withEmail: Email.text!, password: Password.text!) { (user, error) in
                 
-                self.performSegue(withIdentifier: "loginToHome", sender: self)
-            }
-            else{
-                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                
-                alertController.addAction(defaultAction)
-                self.present(alertController, animated: true, completion: nil)
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    self.performSegue(withIdentifier: "loginToHome", sender: self)
+                    
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
             }
         }
     }
-    
-    
+        
+//        Auth.auth().signIn(withEmail: Email.text!, password: Password.text!) { (user, error) in
+//            if error == nil{
+//
+//                self.performSegue(withIdentifier: "loginToHome", sender: self)
+//            }
+//            else{
+//                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+//                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+//            }
+//        }
+//    }
 }
-
--- App/Travel Ticket/Travel Ticket/detailViewController.swift
+++ App/Travel Ticket/Travel Ticket/detailViewController.swift
@@ -13,6 +13,14 @@ import Firebase
 
 class detailViewController: UIViewController {
     
+    
+    @IBOutlet weak var lbTrip: UILabel!
+    @IBOutlet weak var lbPlace1: UILabel!
+    @IBOutlet weak var lbPlace2: UILabel!
+    @IBOutlet weak var lbPlace3: UILabel!
+    @IBOutlet weak var lbPlace4: UILabel!
+    @IBOutlet weak var lbPlace5: UILabel!
+    @IBOutlet weak var lbPrice: UILabel!
     var trip = ""
     
     override func viewDidLoad() {
@@ -21,6 +29,31 @@ class detailViewController: UIViewController {
         // Do any additional setup after loading the view.\
         
         let docRef = Firestore.firestore().collection("TripPromotion").document(trip)
+        
+        docRef.getDocument{ (document, err) in
+            if let document = document {
+                
+            let Place1 = document.get("place1")
+            let Place2 = document.get("place2")
+            let Place3 = document.get("place3")
+            let Place4 = document.get("place4")
+            let Place5 = document.get("place5")
+            let Price = document.get("price")
+            
+                self.lbTrip.text = self.trip as? String
+            self.lbPlace1.text = Place1 as? String
+            self.lbPlace2.text = Place2 as? String
+            self.lbPlace3.text = Place3 as? String
+            self.lbPlace4.text = Place4 as? String
+            self.lbPlace5.text = Place5 as? String
+            self.lbPrice.text = Price as? String
+            }
+            else{
+                print("Document doesn't exist")
+            }
+            
+        }
+        
         print(trip)
     }
     
